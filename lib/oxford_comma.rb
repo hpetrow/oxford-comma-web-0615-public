edafe8a3cd35e8f724f2b37489fd38a78e167a7e
hpetrow <heather.petrow@gmail.com>
2015-06-04 19:20:17 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma (strings)
+  if (strings.length == 1)
+    result = strings[0]
+  elsif (strings.length == 2)
+    result = strings[0] << " and " << strings[1]
+  elsif (strings.length > 2)
+    lastStr = ", and #{strings.pop}"
+    result = strings.join(", ") << lastStr
+  end
+  result
 end
